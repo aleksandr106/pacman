0c14951acf0dbc9b9771922c15efe22b1ddbb141
aleksandr106 <igor.glazov1907@gmail.com>
2016-02-28 22:51:31 +0300

    Update pacman.py

--- pacman.py
+++ pacman.py
@@ -147,18 +147,28 @@ class Ghost(GameObject):
 
 class Pacman(GameObject):
     def __init__(self, x, y, tile_size, map_size):
-        GameObject.__init__(self, './resources/pacman.png', x, y, tile_size, map_size)
+        GameObject.__init__(self, './resources/pacmanright.png', x, y, tile_size, map_size)
         self.direction = 0
         self.velocity = 1
         map.map[self.x][self.y]='p'
+        self.bonys=''
+        self.timer=0
+
 
     def game_tick(self):
+        if self.bonys=='':
+            self.timer=0
+        if (self.bonys=='s' or self.bonys=='i'or self.bonys=='y') and self.timer<=20:
+            self.timer+=1
+        else:
+            self.bonys=''
+            self.timer=0
         super(Pacman, self).game_tick()
         if self.direction == 1:
             self.x += self.velocity
             if self.x > self.map_size-1:
                 self.x = self.map_size-1
-            elif map.map[self.x][self.y]=='w':
+            elif map.map[self.x][self.y]=='w' and self.bonys!='y':
                 self.x-=self.velocity
             else:
                 map.map[self.x][self.y]='p'
@@ -167,7 +177,7 @@ class Pacman(GameObject):
             self.y += self.velocity
             if self.y > self.map_size-1:
                 self.y = self.map_size-1
-            elif map.map[self.x][self.y]=='w':
+            elif map.map[self.x][self.y]=='w' and self.bonys!='y':
                 self.y-=self.velocity
             else:
                 map.map[self.x][self.y]='p'
@@ -176,7 +186,7 @@ class Pacman(GameObject):
             self.x -= self.velocity
             if self.x < 0 :
                 self.x = 0
-            elif map.map[self.x][self.y]=='w':
+            elif map.map[self.x][self.y]=='w' and self.bonys!='y':
                 self.x+=self.velocity
             else:
                 map.map[self.x][self.y]='p'
@@ -185,7 +195,7 @@ class Pacman(GameObject):
             self.y -= self.velocity
             if self.y < 0:
                 self.y = 0
-            elif map.map[self.x][self.y]=='w':
+            elif map.map[self.x][self.y]=='w' and self.bonys!='y':
                 self.y+=self.velocity
             else:
                 map.map[self.x][self.y]='p'
@@ -194,21 +204,25 @@ class Pacman(GameObject):
         self.set_coord(self.x, self.y)
 
 
-def process_events(events, packman):
+def process_events(events, pacman):
     for event in events:
         if (event.type == QUIT) or (event.type == KEYDOWN and event.key == K_ESCAPE):
             sys.exit(0)
         elif event.type == KEYDOWN:
             if event.key == K_LEFT:
-                packman.direction = 3
+                pacman.direction = 3
+                pacman.image=pygame.image.load('./resources/pacmanleft.png')
             elif event.key == K_RIGHT:
-                packman.direction = 1
+                pacman.direction = 1
+                pacman.image=pygame.image.load('./resources/pacmanright.png')
             elif event.key == K_UP:
-                packman.direction = 4
+                pacman.image=pygame.image.load('./resources/pacmanup.png')
+                pacman.direction = 4
             elif event.key == K_DOWN:
-                packman.direction = 2
+                pacman.image=pygame.image.load('./resources/pacmandown.png')
+                pacman.direction = 2
             elif event.key == K_SPACE:
-                packman.direction = 0
+                pacman.direction = 0
 
 
 class Wall(GameObject):
@@ -220,6 +234,16 @@ class Wall(GameObject):
             map.map[x][y]='rw'
             self.image=pygame.transform.rotate(self.image,270)
 
+class Food(GameObject):
+    def  __init__(self,x,y,tile_size, map_size):
+        GameObject.__init__(self, './resources/b.png', x, y, tile_size, map_size)
+
+class Bonys(GameObject):
+    def  __init__(self,x,y,b, img,tile_size, map_size):
+        GameObject.__init__(self, img, x, y, tile_size, map_size)
+        self.b=b
+
+
 if __name__ == '__main__':
     input=open('input.txt','r')
     init_window()
@@ -227,6 +251,8 @@ if __name__ == '__main__':
     map_size =16
     walls=[]
     ghosts=[]
+    food=[]
+    bonys=[]
     map=Map(map_size,map_size)
     for i in range(map_size+1):
         for j in range(map_size+1):
@@ -234,8 +260,18 @@ if __name__ == '__main__':
             print(a)
             if a=='n':
                 walls.append(Wall(j, i, 'n', tile_size, map_size))
-    ghosts.append(Ghost(8, 9, tile_size, map_size))
-    ghosts.append(Ghost(1, 0, tile_size, map_size))
+            elif a=='r':
+                walls.append(Wall(j, i, 'rw', tile_size, map_size))
+            elif a=='f':
+                food.append(Food(j,i,tile_size,map_size))
+            elif a=='g':
+                ghosts.append(Ghost(j, i, tile_size, map_size))
+            elif a=='s':
+                bonys.append(Bonys(j,i,'s',"./resources/s.png",tile_size,map_size))
+            elif a=='i':
+                bonys.append(Bonys(j,i,'i',"./resources/i.png",tile_size,map_size))
+            elif a=='y':
+                bonys.append(Bonys(j,i,'y',"./resources/b.png",tile_size,map_size))
     pacman = Pacman(5, 5, tile_size, map_size)
     background = pygame.image.load("./resources/background.png")
     screen = pygame.display.get_surface()
@@ -247,14 +283,42 @@ if __name__ == '__main__':
         for g in ghosts:
             g.game_tick()
         draw_background(screen, background)
-        for w in walls:
-            if w.x==pacman.x and w.y==pacman.y:
-               walls.pop(walls.index(w))
-            w.draw(screen)
+        i=0
+        while i<len(walls):
+            walls[i].draw(screen)
+            if walls[i].x==pacman.x and walls[i].y==pacman.y:
+                walls.pop(i)
+            else:
+                i+=1
+        j=0
+        while j<len(food):
+            food[j].draw(screen)
+            if food[j].x==pacman.x and food[j].y==pacman.y:
+                food.pop(j)
+            else:
+                j+=1
+        k=0
+        while k<len(bonys):
+            bonys[k].draw(screen)
+            if bonys[k].x==pacman.x and bonys[k].y==pacman.y:
+                if bonys[k].b=='s':
+                    pacman.bonys='s'
+                elif bonys[k].b=='i':
+                    pacman.bonys='i'
+                else:
+                    pacman.bonys='y'
+                pacman.timer=0
+                bonys.pop(k)
+            else:
+                k+=1
         pacman.draw(screen)
-        for g in ghosts:
-            g.draw(screen)
-        pygame.display.update()
-        for g in ghosts:
-            if g.x==pacman.x and g.y==pacman.y:
+        g=0
+        while g<len(ghosts):
+            ghosts[g].draw(screen)
+            if pacman.x==ghosts[g].x and ghosts[g].y==pacman.y and pacman.bonys=='s':
+                ghosts.pop(g)
+            elif ghosts[g].x==pacman.x and ghosts[g].y==pacman.y and pacman.bonys!='i':
                 sys.exit(0)
+            else:
+                g+=1
+        pygame.display.update()
